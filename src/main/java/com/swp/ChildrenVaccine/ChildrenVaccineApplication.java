5429071fbe298b0273a9d08067e31477877d534c
b-baocode <161267408+b-baocode@users.noreply.github.com>
2025-02-16 14:19:47 +0700

    merge all to main

--- src/main/java/com/swp/ChildrenVaccine/ChildrenVaccineApplication.java
+++ src/main/java/com/swp/ChildrenVaccine/ChildrenVaccineApplication.java
@@ -8,6 +8,7 @@ public class ChildrenVaccineApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChildrenVaccineApplication.class, args);
+		System.out.println("Hello World");
 	}
 
 }
