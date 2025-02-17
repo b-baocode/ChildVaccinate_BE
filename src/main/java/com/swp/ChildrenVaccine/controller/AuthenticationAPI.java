b5202e0b3195751c355062c54c28b72d17bb284c
b-baocode <161267408+b-baocode@users.noreply.github.com>
2025-02-17 07:58:33 +0700

    add controller package(authen)

---
+++ src/main/java/com/swp/ChildrenVaccine/controller/AuthenticationAPI.java
@@ -0,0 +1,13 @@
+package com.swp.ChildrenVaccine.controller;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class AuthenticationAPI {
+}
